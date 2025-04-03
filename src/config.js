af1262d1b8e1a5501fb132efe588278f7b79dbe0
pewpewpewbumbum <zyraonetrickpony@gmail.com>
2025-04-03 18:53:53 +0200

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export default {
+  api: {
+    url: "https://api.deepseek.com/v1",
+  },
+  headers: {
+    "Authorization": "sk-2372e367568740719bfe2e24981c05ef",
+    "Content-Type": "application/json"
+  }
+};
